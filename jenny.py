7a61c8fb03bcfef513f51f918d4dd3cb44886259
derpyzza <derpyzza@gmail.com>
2022-05-24 23:45:23 +0500

    Minor Refactors
    cleans things up into functions, and adds header link functionalities :>

--- jenny.py
+++ jenny.py
@@ -5,21 +5,14 @@ import glob
 import markdown
 import commonmark
 
-# generate an index.html file if it does not exist yet
-# read an index template, and 
-posts = []
+# DOING read config files
+    # what kinda configs?? idk
+# DONE read in markdown files
+# DONE read in template file
+# DONE convert markdown to html
+# DONE write html out using the template
 
-if not os.path.exists( 'public' ):
-    os.mkdir( 'public' )
-
-
-for f in glob.iglob( 'src/*.md' ):
-    content = ''
-    title = ''
-    date = ''
-    subtitle = ''
-    template = open('assets/template.html', 'r').read()
-    with open( f, 'r' ) as file:
+def preprocess_file(file):
         # preprocess the file for metadata. 
         # metadata in this implementation just starts with a "$" symbol, followed by a command name and command args
         # current commands are:
@@ -29,67 +22,70 @@ for f in glob.iglob( 'src/*.md' ):
         # subtitle: same as title, but smaller and longer. kind of a summary in a way.
         # date:     the date on which the post was published or written. to be displayed at the top of the document and also
         #           the main page
-        for line in file:
-            stripped = line.lstrip()
+    content=''
+    title=''
+    subtitle=''
+    date=''
+    for line in file:
+        stripped = line.lstrip()
 
-            if stripped.startswith('$'):
-                command,_,args = stripped.rstrip('\n').lstrip('$').partition(' ')
-                args = args.strip()
+        if stripped.startswith('$'):
+            command,_,args = stripped.rstrip('\n').lstrip('$').partition(' ')
+            args = args.strip()
 
-                if command == 'title':
-                    title = args
-                elif command == 'date':
-                    date = args
-                elif command == 'subtitle':
-                    subtitle = args
-            elif stripped.startswith('\\'):
-                content += stripped[2:]
-            else:
-                content += line
-        raw = file.read()
-        html = markdown.markdown( content, extensions=[ 'extra', 'codehilite', 'toc', 'superscript'] )
-        # print( "file: " + f + " content: \n" + content )
+            if command == 'title':
+                title = args
+            elif command == 'date':
+                date = args
+            elif command == 'subtitle':
+                subtitle = args
+        elif stripped.startswith('\\'):
+            content += stripped[2:]
+        elif stripped.startswith('##'):
+            id = stripped.rstrip('\n').lstrip('##').lstrip(' ')
+            content += "<h2 id=\"" + id.replace(" ", "-").lower() + "\">" + id + "</h2>"
+        else:
+            content += line
+    return {"content": content, "title": title, "subtitle": subtitle, "date": date}
+# PREPROCES FILE END
+
+def format_file(post, template):
+    template = template.replace("{{content}}", post['content'])
+    template = template.replace("{{title}}", post['title'])
+    template = template.replace("{{subtitle}}", post['subtitle'])
+    template = template.replace("{{date}}", post['date'])
+    return template
+
+posts = []
+if not os.path.exists( 'public' ):
+    os.mkdir( 'public' )
+
+for f in glob.iglob( 'src/*.md' ):
+    post = {}
+    template = open('assets/template.html', 'r').read()
+    with open( f, 'r' ) as file:
+        post = preprocess_file(file)
+        post['content'] = markdown.markdown( post["content"], extensions=[ 'extra', 'codehilite', 'toc', 'superscript'] )
 
     file_name = os.path.basename( f )
     destination = os.path.join( "public", os.path.splitext( file_name )[ 0 ] + ".html" )
-    # print( f + " title = " + title + " date = " + date + " subtitle = " + subtitle )
-    # print(f + " : " + destination)
+    post["dest"] = destination
 
     with open( destination, 'w' ) as file:
-        template = template.replace("{{content}}", html)
-        template = template.replace("{{title}}", title)
-        template = template.replace("{{date}}", date)
-        template = template.replace("{{subtitle}}", subtitle)
-        # print(f + "/" + destination + " : \n" + template)
+        template = format_file(post, template)
         file.write(template)
-    posts.append({  "post"      : destination,
-                    "title"     : title,
-                    "date"      : date,
-                    "subtitle"  : subtitle })
+    posts.append(post)
 
 index = open('assets/index_template.html', 'r').read()
 content = ''
 
 for post in posts:
-    content += "<a href=\"" + post['post'] + "\" class=\"post\">"
+    content += "<a href=\"" + post['dest'] + "\" class=\"post\">"
     content += "<div class=\"content\">" + "<div class=\"post-title\">"
     content += "<h2>" + post['title'] + "</h2>"
     content += "<h4>" + post['date'] + "</h4>"
     content += "</div> <p>" + post['subtitle'] + "</p> </div> </a>"
 
-# post = '''<a href="./public/00_test.html">
-#     <div class="content">
-#     <div class="post-title">
-#         <h2>{{title}}</h2>
-#         <h4>{{date}}</h4>
-#     </div>
-#     <p>{{subtitle}}</p>
-#     </div>
-#     </a>
-# '''
-
 with open ('index.html', 'w') as file:
     index = index.replace("{{posts}}", content)
     file.write(index)
-
-print(posts)
