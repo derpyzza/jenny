1fcfadfa66b4cafef17879c89f37823d5b509e69
derpyzza <derpyzza@gmail.com>
2023-03-20 15:53:24 +0500

    Implements basic functionality
    forgot to commit again, whoops.
    + Reads list of markdown files from source directory and returns file
      names
    + loops over list of files, and:
    + preprocesses them for metadata. currently the metadata is hardcoded
      and supports the following variables: "title": the post title,
      "subtitle": the post subtitle, "date": the date the post was written,
      "tags": list of tags for the post. every line that isnt a metadata
      variable is considered a line in the post's content itself and is
      ignored. later intend to make the metadata customizable
    + converts the markdown content into html content
    + uses a template html file and the contents of a .md file to generate
      the final webpage
    + writes it out to a public directory.
    
    no indexing support currently, that's for next commit :)

--- TODO.md
+++ TODO.md
@@ -3,9 +3,10 @@
 [ ] Migrate python script to Go
 
 	[X] Initialize project ;)
-	[ ] Read markdown files in source folder.
-	[ ] Convert markdown to html files.
-	[ ] Feed html data into templates and generate final html file with templates.
-	[ ] Place final html file into Public folder.
+	[X] Read markdown files in source folder.
+	[X] Preprocess source files and extract metadata from them.
+	[X] Convert markdown to html files.
+	[X] Feed html data into templates and generate final html file with templates.
+	[X] Place final html file into Public folder.
 	[ ] Update Index page with the latest html file.
 	[ ] Add support for config files.
--- main.go
+++ main.go
@@ -1,9 +1,218 @@
 package main
 
 import (
-	"fmt"
+	// stdlib
+	"bufio"
+	// "fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	// third-party
+	"github.com/charmbracelet/log"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
 )
 
+// source directory, should contain markdown (.md) files :)
+const SRCDIR string = "./src/";
+const TEMPDIR string = "./templates/";
+const PUBDIR string = "./public/";
+
+type Post struct {
+	title string;
+	subtitle string;
+	date string;
+	tags string;
+	content string;
+}
+
+/*
+	Iterates through the source directory,
+	and checks every file to see if it's a markdown
+	file. If so, it stuffs the file's name into an array and returns
+	the array of file names.
+*/
+func GetSourceFiles() []string {
+	files, err := os.ReadDir(SRCDIR);
+
+	var srcFiles []string;
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	log.Info("Fetching Source Files");
+	for _, file := range files {
+		log.Info("Found", "file", file.Name());
+		if filepath.Ext(file.Name()) == ".md" {
+			// fmt.Println("Correct format!");
+			fileName := strings.TrimRight(file.Name(), ".md");
+			srcFiles = append(srcFiles, fileName);
+		}
+	}
+	return srcFiles;
+}
+
+/*
+Loops over every line in a given file and 
+searches for the template metadata. template
+metadata is defined inside a block with a
+beginning and an end marker in order to be able 
+to quickly exit out of a file when all the 
+relevant data has been extracted from the file.
+
+metadata begins with an "@" symbol, followed immediately
+by the key name, followed by a "<-" marker, and then everything
+after that until a newline is accepted as the value.
+*/
+func PreprocessFile(file string) Post {
+
+// messy, TODO: clean
+
+	post := Post{};
+
+	path := SRCDIR + file + ".md"
+	rawFile, err := os.Open(path);
+	if err != nil {
+		log.Fatal(err);
+	}
+
+	scanner := bufio.NewScanner(rawFile);
+
+	for scanner.Scan() {
+		line := scanner.Text();
+		str := strings.TrimRight(line, " ");
+
+		if strings.HasPrefix(str, "@") {
+			str = strings.TrimLeft(str, "@");
+			str = strings.TrimRight(str, "\t");
+			split := strings.Split(str, "<-");
+			command := split[0];
+			command = strings.TrimRight(command, "\t");
+			command = strings.TrimRight(command, " ");
+
+			if command == "title" {
+				log.Debug("Command found, 'TITLE'", "cmd", command );
+				post.title = string(split[1])
+			} else if command == "subtitle" {
+				log.Debug("Command found, 'SUBTITLE'", "cmd", command );
+				post.subtitle = string(split[1])
+			} else if command == "date" {
+				log.Debug("Command found, 'DATE'", "cmd", command );
+				post.date = string(split[1])
+			} else if command == "tags" {
+				log.Debug("Command found, 'TAGS'", "cmd", command );
+				post.tags = string(split[1])
+			} else {
+				log.Debug("Unknown command", "cmd", command );
+			}
+		} else {
+			post.content = post.content + "\n" + line;
+		}
+	}
+
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+
+	// log.Info("", "POST", post);
+	return post;
+}
+
+/*
+Takes in a file, 
+*/
+func FormatContent(content string) string {
+
+	// path := SRCDIR + file + ".md"
+	// rawFile, err := os.ReadFile(path);
+	// if err != nil {
+	// 	log.Fatal(err);
+	// }
+	log.Info("Converting to markdown")
+	md := []byte(content)
+	md = markdown.NormalizeNewlines(md);
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs;
+	p := parser.NewWithExtensions(extensions);
+
+	doc := p.Parse(md);
+
+	// create HTML renderer
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	html := markdown.Render(doc, renderer)
+
+	return string(html[:]);
+}
+
+func TemplateFile(post Post, template string) (string, error) {
+	path := TEMPDIR + template + ".html"
+	templateRaw, err := os.ReadFile(path);	
+
+	log.Info("Templating file");
+	postFile := string(templateRaw[:]);
+
+	postFile = strings.Replace(postFile, "{{content}}", post.content, 1);
+	postFile = strings.Replace(postFile, "{{title}}", post.title, 1);
+	postFile = strings.Replace(postFile, "{{subtitle}}", post.subtitle, 1);
+	postFile = strings.Replace(postFile, "{{date}}", post.date, 1);
+	postFile = strings.Replace(postFile, "{{tags}}", post.tags, 1);
+	postFile = strings.TrimRight(postFile, "\n");
+
+	// fmt.Println(postFile)
+	
+	return postFile, err;
+}
+
+// meat and bones of the app;
+func jennyrate() {
+	files := GetSourceFiles();
+
+	for _, f := range files {
+		log.Info("Jennyrating ", "file", f);
+		post := PreprocessFile(f);
+		post.content = FormatContent(post.content);
+
+		html, err := TemplateFile(post, "post");
+		if err != nil {
+			log.Error(err);
+			continue;
+		}
+
+		path := PUBDIR + f + ".html";
+		file, ferr := os.Create(path)
+		if ferr != nil {
+			log.Error(ferr);
+			continue;
+		}
+
+		defer file.Close();
+
+		_, err = file.WriteString(html);
+		if err != nil {
+			log.Error(err);
+			continue;
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Jenny: Hello from Go!");
+	log.Info("Initialized jenny");
+	jennyrate();
+	// post := Post {
+	// 	title: "Post title",
+	// 	subtitle: "Post subtitle",
+	// 	date: "23-03-20",
+	// 	tags: "#hello #there",
+	// 	content: "this is post content :)",
+	// }
+	// TemplateFile(post, "post")
+	//
+	// PreprocessFile("other")
+
 }
